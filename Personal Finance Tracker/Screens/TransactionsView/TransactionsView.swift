37bf86e0af55df764dc0e8346c9407ea493c90bc
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-08-19 21:00:29 -0600

    Added "Add" butto to transactions view, Added segments.

--- Personal Finance Tracker/Screens/TransactionsView/TransactionsView.swift
+++ Personal Finance Tracker/Screens/TransactionsView/TransactionsView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct TransactionsView: View {
+    @State private var showAddTransactionView: Bool = false
     var body: some View {
         NavigationStack{
             List{
-                NavigationLink {
-                    AddTransactionView()
-                } label: {
-                    Text("Example transaction")
+                Section(header: Text("Total transactions")){
+                    
+                }
+                Section(header: Text("Transactions")){
+                    HStack{
+                        Image(systemName: "leaf")
+                        Text("Example transaction")
+                    }
                 }
             }
+            .sheet(isPresented: $showAddTransactionView){
+                AddTransactionView()
+            }
             .navigationTitle("Transactions")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing){
+                    Button{
+                        showAddTransactionView.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.headline)
+                            .foregroundStyle(.accent)
+                    }
+                }
+            }
+            
         }
         
     }
