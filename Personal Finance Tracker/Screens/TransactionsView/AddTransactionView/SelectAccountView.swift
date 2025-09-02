f081e245a07340805a20c3b7bd69536f66125d4f
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-09-01 18:24:20 -0600

    Created dividers for select account view for different accounts

--- Personal Finance Tracker/Screens/TransactionsView/AddTransactionView/SelectAccountView.swift
+++ Personal Finance Tracker/Screens/TransactionsView/AddTransactionView/SelectAccountView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 struct SelectAccountView: View {
     @Binding var selectedAccount: Account?
     @State private var accounts: [Account] = MockData.mockAccounts  //Turn this to environment variable
+    @State private var columnLayout = Array(repeating: GridItem(), count: 2)
+    
     var body: some View {
         NavigationStack{
             ScrollView {
-           
+                VStack(alignment: .leading){
+                    HStack{Spacer()}
+                    Text("Cash")
+                        .foregroundStyle(.secondary)
+                        .font(.title2)
+                    
+                    LazyVGrid(columns: columnLayout){
+                        ZStack{
+                            Capsule()
+                                .foregroundStyle(.red)
+                            Circle()
+                                .frame(width: 40, height: 40)
+                        }
+                    }
+                    Text("Accounts")
+                        .foregroundStyle(.secondary)
+                        .font(.title2)
+                    
+                    Text("Credit Cards")
+                        .foregroundStyle(.secondary)
+                        .font(.title2)
+                }
+                
                 ForEach(accounts, id: \.self){ account in
                     Button{
                         selectedAccount = account
@@ -23,6 +47,7 @@ struct SelectAccountView: View {
                 }
                 
             }
+            .padding(.horizontal)
             .navigationBarTitle("Select Account")
         }
                 
