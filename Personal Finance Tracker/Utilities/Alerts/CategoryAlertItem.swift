333ab7bbab57ee0fe1f4da5e0cc6041abfde26cc
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-09-05 11:00:55 -0600

    Created AddTransactionView AlertContexts.

--- Personal Finance Tracker/Utilities/Alerts/CategoryAlertItem.swift
+++ Personal Finance Tracker/Utilities/Alerts/CategoryAlertItem.swift
@@ -15,7 +15,47 @@ struct CategoryAlertItem: Identifiable {
 }
 
 struct AlertContext {
+    
+    // MARK: CreateCategoryView Alerts
     static let categoryNameIsRequired: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not create category"),
-                                                                             alertMessage: Text("Category name is required"),
+                                                                             alertMessage: Text("Category name is required."),
                                                                              alertDismissButton: .default(Text("OK")))
+    
+    // MARK: AddTransactionView Alerts
+    
+    //Transaction label is empty
+    static let transactionLabelIsEmpty: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                              alertMessage: Text("Transaction should be named before adding. Please enter a name."),
+                                                                              alertDismissButton: .default(Text("OK")))
+    
+    // Transaction type is not selected
+    static let transactionTypeNotSelected: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                                 alertMessage: Text("Transaction cannot be added without a type. Please select a transaction type."),
+                                                                                 alertDismissButton: .default(Text("OK")))
+    //Currency is nil
+    static let currencyIsEmpty: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                    alertMessage: Text("There was an issue adding the transaction. Please verify a currency has been selected."),
+                                                                    alertDismissButton: .default(Text("OK")))
+    
+    // amount is less or equal to zero
+    static let invalidAmount: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                    alertMessage: Text("Please give a proper amount to the transaction. The amount will be added depending on the transaction type."),
+                                                                    alertDismissButton: .default(Text("OK")))
+    
+    // Date is nil
+    static let dateIsEmpty: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                  alertMessage: Text("Please select a proper date for the transaction."),
+                                                                  alertDismissButton: .default(Text("OK")))
+    
+    // Category is nil
+    static let categoryIsEmpty: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                      alertMessage: Text("Please select a category for the transaction."),
+                                                                      alertDismissButton: .default(Text("OK")))
+    
+    // Account is nil
+    static let accountIsEmpty: CategoryAlertItem = CategoryAlertItem(alertTitle: Text("Could not add transaction"),
+                                                                     alertMessage: Text("Please select a target account for the transaction."),
+                                                                     alertDismissButton: .default(Text("OK")))
+    
 }
+    
