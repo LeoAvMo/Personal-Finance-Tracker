a40943bc5d0b22c42844711e109a0a9f9f7e083c
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-09-22 23:00:46 -0600

    Created categories view.

---
+++ Personal Finance Tracker/Screens/Categories/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesView.swift
+//  Personal Finance Tracker
+//
+//  Created by Leo A.Molina on 22/09/25.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesView()
+}
