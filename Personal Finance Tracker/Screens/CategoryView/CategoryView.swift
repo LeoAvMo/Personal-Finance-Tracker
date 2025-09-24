b5dcb5cfe438273a68ada145ff163213b434f66d
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-09-23 20:30:17 -0600

    Made category view for editing categories.

---
+++ Personal Finance Tracker/Screens/CategoryView/CategoryView.swift
@@ -0,0 +1,46 @@
+//
+//  CategoriesView.swift
+//  Personal Finance Tracker
+//
+//  Created by Leo A.Molina on 22/09/25.
+//
+
+import SwiftUI
+import SwiftData
+
+
+// TODO: Make option to move all category transactions to another category or completely delete all transactions when category is erased.
+struct CategoryView: View {
+    @Environment(\.modelContext) private var modelContext
+    
+    @Query(sort: \Category.name) private var categories: [Category]
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(categories) { category in
+                    HStack{
+                        CategoryIconView(category: category, showLabel: false, isSelected: false)
+                        Text(category.name)
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(category.color)
+                    }
+                }
+            }
+            .toolbar {
+                NavigationLink{
+                    AddCategoryView()
+                } label: {
+                    Button("Add Category", systemImage: "plus"){ }
+                }
+            }
+            .navigationTitle(Text("Categories"))
+        }
+    }
+}
+
+#Preview {
+    CategoryView()
+        .modelContainer(for: Category.self, inMemory: true)
+}
