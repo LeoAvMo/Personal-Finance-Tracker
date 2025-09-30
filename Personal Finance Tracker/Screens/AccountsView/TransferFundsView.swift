b4ee512d1ebd3d582a4a673645698ea787d67dc4
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-09-30 18:30:13 +0200

    Added checks before making the transfer, put a toolbar to transfer.

--- Personal Finance Tracker/Screens/AccountsView/TransferFundsView.swift
+++ Personal Finance Tracker/Screens/AccountsView/TransferFundsView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 import SwiftData
 
+// TODO: Add taxes in case of different currencies.
 struct TransferFundsView: View {
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
     @Query(sort: \Account.name) var accounts: [Account]
     @Query(sort: \Currency.name) var currencies: [Currency]
     
@@ -28,6 +30,7 @@ struct TransferFundsView: View {
     
     var body: some View {
         NavigationStack {
+            
             Form {
                 Section(header: Text("Source & Destination")) {
                     Picker(selection: $sourceAccount, label: Text("Source")) {
@@ -67,26 +70,48 @@ struct TransferFundsView: View {
                     }
                 }
                 
-                if currenciesAreDifferent {
-                    Section(header: Text("Conversion")) {
-                        HStack{
-                            // Make conversion amount be calculated when this section apepars (Selecting amount)
-                            TextField("$", value: $conversionAmount, format: .currency(code: sourceAccount?.currency.code ?? ""))
-                                .keyboardType(.decimalPad)
-                                .multilineTextAlignment(.trailing)
-                                .fontWeight(.semibold)
-                                .foregroundStyle(.primary)
+                if !currenciesAreDifferent {
+                    Section(header: Text("Summary")) {
+                        
+                            HStack{
+                                Spacer()
+                                // Make conversion amount be calculated when this section apepars (Selecting amount)
                                 
-                            Image(systemName: "chevron.right.2")
-                            
-                            TextField("$", value: $amount, format: .currency(code: destinationAccount?.currency.code ?? ""))
-                                .keyboardType(.decimalPad)
-                                .multilineTextAlignment(.trailing)
-                                .fontWeight(.semibold)
-                                .foregroundStyle(.primary)
-                        }
+                                VStack{
+                                    
+                                    Text("-\(amount, format: .currency(code: sourceAccount?.currency.code ?? ""))")
+                                        .foregroundStyle(.red)
+                                        .font(.caption)
+                                    
+                                    Text("\((sourceAccount?.balance ?? amount) - amount, format: .currency(code: sourceAccount?.currency.code ?? ""))")
+                                        .foregroundStyle(sourceAccount?.color ?? .primary)
+                                        .fontWeight(.semibold)
+                                    
+                                    Text("\(sourceAccount?.name ?? "No account")")
+                                        .foregroundStyle(.secondary)
+                                }
+                        
+                                Image(systemName: "chevron.right.2")
+                                
+                                VStack {
+                                    Text("+\(amount, format: .currency(code: destinationAccount?.currency.code ?? ""))")
+                                        .foregroundStyle(.green)
+                                        .font(.caption)
+                                    
+                                    Text("\((destinationAccount?.balance ?? 0) + amount, format: .currency(code: destinationAccount?.currency.code ?? ""))")
+                                        .foregroundStyle(destinationAccount?.color ?? .primary)                                .foregroundStyle(destinationAccount?.color ?? .primary)
+                                        .fontWeight(.semibold)
+                                        Text("\(destinationAccount?.name ?? "No account")")
+                                            .foregroundStyle(.secondary)
+                                }
+                                Spacer()
+                            }
+                        
+                        
                     }
                 }
+                
+                
                 
             }
             .navigationTitle(Text("Transfer Funds"))
@@ -94,7 +119,45 @@ struct TransferFundsView: View {
                 sourceAccount = accounts.first
                 destinationAccount = accounts.last
             }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction){
+                    Button("Transfer", systemImage: "checkmark", action: transfer)
+                }
+            }
         }
+    }
+    
+    private func transfer() {
+        
+        if currenciesAreDifferent {
+            // set alertItem
+            showAlert = true
+            return
+        }
+        
+        if sourceAccount == destinationAccount {
+            showAlert = true
+            return
+        }
+        
+        if sourceAccount == nil || destinationAccount == nil {
+            showAlert = true
+            return
+        }
+        
+        if sourceAccount?.balance ?? 0 < amount {
+            showAlert = true
+            return
+        }
+        
+        if amount == 0 || amount.isNaN || amount.isInfinite {
+            showAlert = true
+            return
+        }
+        
+        sourceAccount!.balance -= amount
+        destinationAccount!.balance += amount
+        dismiss()
     }
 }
 
